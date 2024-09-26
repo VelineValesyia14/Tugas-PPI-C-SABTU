d675686d5dedd614e12b0dc7710bdc8d97228b23
VelineValesyia14 <vvalesyia1404@gmail.com>
2024-09-26 23:58:51 +0700

    Add files via upload

---
+++ TugasIndividu(1).cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (){
+	
+	printf("		STT MANDALA				\n");
+	
+	printf(".............................................\n");
+	
+	printf("Nama	:	Veline Valesyia\n");
+	
+	printf("NIM     :	2441048\n");
+	
+	printf("Prodi	:	Teknik Informatika\n");
+	
+	return 0;
+}
---
+++ TugasIndividu(2).cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main ()
+{
+	int PJ, LR, LU ;
+	
+	
+	PJ=10;
+	
+	LR=7;
+	
+	printf("Di ketahui panjang  persegi 10CM\n");
+	printf("Dan lebar persegi  7CM\n");
+	printf("Maka luas persegi adalah?\n");
+	
+	LU=PJ*LR;
+	
+	printf("%d",LU);
+	
+	return 0;
+	
+}
